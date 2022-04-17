f9f6a90f85cb0ff47946e26c78e1bf5269fc8731
kr673 <1228793219@qq.com>
2022-04-18 00:20:44 +0800

    data processing

--- test.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-wb = openpyxl.load_workbook('people_low.xlsx')
-# print(wb.sheetnames)
-# print(wb.active)
-sheet = wb['Sheet1']
-
-for row in sheet.rows:
-    print([cell.value for cell in row])
